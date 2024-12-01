10f2e974d6a32078b03bdc8716081d40c23893ee
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2024-12-01 10:24:33 -0500

    complete part 2

--- day1.py
+++ day1.py
@@ -7,16 +7,36 @@ def countdistance(filename : str) -> int:
 
   for line in lines:
     ap1, ap2 = line.split()
-    colr.append(ap1)
-    coll.append(ap2)
+    colr.append(int(ap1))
+    coll.append(int(ap2))
+
   colr.sort()
   coll.sort()
+  
   for i in range(0, len(colr)):
-    distance += abs(int(colr[i]) - int(coll[i]))
+    distance += abs(colr[i] - coll[i])
 
   return distance
 
 def similarityscore(filename : str) -> int:
-  return 0
+  dictr : dict[int, int] = {}
+  coll : list[int] = []
+  score : int = 0
+  with open(filename) as f:
+    lines : list = f.readlines()
+
+  for line in lines:
+    ap1, ap2 = line.split()
+    coll.append(int(ap2))
+    if int(ap1) not in dictr:
+      dictr[int(ap1)] = 1
+    else:
+      dictr[int(ap1)] += 1
+  
+  for idx, elem in enumerate(coll):
+    score += dictr.get(int(elem), 0)*elem
+  
+  return score
 
 print(countdistance('./inputs/d1input.txt'))
+print(similarityscore('./inputs/d1input.txt'))
