2c8beb578226aae7d81a91e38bb29f8216067625
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2024-12-03 03:02:47 -0500

    regex regex regex

---
+++ day3.py
@@ -0,0 +1,26 @@
+import re
+
+with open('./inputs/d3input.txt') as f:
+  data = f.read()
+
+matches = re.findall("mul\(\d{1,3}\,\d{1,3}\)", data)
+mulsum = 0
+for match in matches:
+  a, b = map(int, match[4:-1].split(','))
+  mulsum += a*b
+print(mulsum)
+
+#matches2 = re.findall("don\'t\|mul\(\d{1,3}\,\d{1,3}\)|do\(\)", data)
+matches2 = re.findall("mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don\'t\(\)", data)
+mulsum = 0
+doflag = True
+for match2 in matches2:
+  if match2 == "do()":
+    doflag = True
+  elif match2 == "don't()":
+    doflag = False
+  else:
+    if doflag:
+      a, b = map(int, match2[4:-1].split(','))
+      mulsum += a*b
+print(mulsum)
