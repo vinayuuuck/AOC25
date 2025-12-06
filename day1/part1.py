c92bead20e13238d9b2769bae98b4f902cec27ba
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2025-12-06 00:52:35 -0500

    LETS GOOOOOOO

---
+++ day1/part1.py
@@ -0,0 +1,20 @@
+def main(rotations):
+    current = 50
+    onzero = 0
+    for r in rotations:
+        dir, amt = r[0], int(r[1:])
+        if dir == "L":
+            rem = (current - amt) % 100
+            q = amt // 100
+            current = rem
+        else:
+            rem = (current + amt) % 100
+            q = amt // 100
+            current = rem
+
+    return onzero
+
+
+if __name__ == "__main__":
+    with open("./input.txt") as f:
+        print(main(f.read().split("\n")[:-1]))
